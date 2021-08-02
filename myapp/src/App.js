4bf550db56a3abc0e9dd7bfb7ca5e7ae8acdca87
KONDO, Hiroyasu <hrknd@users.noreply.github.com>
2021-08-02 19:36:07 +0900

    more jsx

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,21 @@
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>Helo</div>
-  );
+// function App() {
+//   return (
+//     <h1>Helo</h1>
+//   );
+// }
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <label htmlFor="foo">bar</label>
+        <input type="text" onChange={() => {console.log("changed")}} />
+      </React.Fragment>
+    );
+  }
 }
+
 
 export default App;
