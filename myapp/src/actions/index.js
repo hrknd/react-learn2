754a2445ac12342cb1ad1c39ba2f7eb29455b9f0
KONDO, Hiroyasu <hrknd@users.noreply.github.com>
2021-08-03 11:21:39 +0900

    redux basic

---
+++ myapp/src/actions/index.js
@@ -0,0 +1,14 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => {
+    return {
+        type:'INCREMENT'
+    }
+}
+
+export const dencrement = () => {
+    return {
+        type:'DECREMENT'
+    }
+}
